8ba7d1c3c3371fde4a1c94d718a7a2789344cb2e
GenLee <cmp_hpp@hotmail.com>
2016-09-13 23:17:20 +0200

    Create class connection factory

---
+++ src/com/genlee/jdbc/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package com.genlee.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() {
+
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/genleeagenda1", "root", "root");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+
+		}
+	}
+
+}
